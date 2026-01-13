f232d0d063d2dbf8886bc0900f1b293777aa6eff
aka-Kiet <kietdzv@gmail.com>
2026-01-13 09:14:23 +0700

    code new

--- DoAnMonHoc/app/Http/Controllers/ProductController.php
+++ DoAnMonHoc/app/Http/Controllers/ProductController.php
@@ -3,12 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book; // Import Model
 
 class ProductController extends Controller
 {
-    public function index() {
+    public function index(Request $req) {
         $viewData = [];
         $viewData["title"] = "Sản phẩm";
+
+        $products = Book::where('is_active', true)
+                            ->with('author')
+                            ->orderBy('created_at', 'desc')
+                            ->paginate(9);
+
+        $bestSellers = Book::where('is_active', true)
+                                ->orderBy('sold_quantity', 'desc')
+                                ->take(4)
+                                ->get();
+
+        $viewData["products"] = $products;
+        $viewData["bestSellers"] = $bestSellers;
 
         return view('product.index')->with("viewData", $viewData);
     }
